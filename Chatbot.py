5ec6e131b1028884170cfc471a4a8d6585367c0e
cedim1 <164538920+cedim1@users.noreply.github.com>
2024-04-10 20:17:07 +0100

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,9 +1,9 @@
 prompt = ["Hello", "hi", "bye", "bye"] #insert more prompts and responses here for it to say
 print("how can I help?")
 def chat():
-pro = input()
-numberof = prompt.index(pro)
-print(prompt[numberof + 1])
-chat()
-return
+  pro = input()
+  numberof = prompt.index(pro)
+  print(prompt[numberof + 1])
+  chat()
+  return
 chat()
